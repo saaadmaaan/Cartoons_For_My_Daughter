3d1430d5cfaa933d27286e2cd5a18c2ce78bf84f
saaadmaaan <alika.nozadze42@gmail.com>
2024-12-06 08:07:57 -0500

    website

---
+++ scripts.js
@@ -0,0 +1,23 @@
+// Function to play video
+function playVideo(fileId) {
+  const iframe = document.getElementById('videoIframe');
+  const videoPlayer = document.getElementById('videoPlayer');
+
+  // Set the source of the iframe to the Google Drive video embed URL
+  iframe.src = `https://drive.google.com/file/d/${fileId}/preview`;
+
+  // Show the video player
+  videoPlayer.style.display = 'flex';
+}
+
+// Function to close the video player
+function closeVideoPlayer() {
+  const iframe = document.getElementById('videoIframe');
+  const videoPlayer = document.getElementById('videoPlayer');
+
+  // Clear the iframe source to stop the video
+  iframe.src = '';
+
+  // Hide the video player
+  videoPlayer.style.display = 'none';
+}
